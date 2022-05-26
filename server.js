c192612cdd0427ee574b7158eb11369409e31a12
Βασιλης Καλυβας <bkalivas8@gmail.com>
2022-05-26 19:15:52 +0300

    make it better

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
-const server = app.listen(5000, console.log(`Server is running on PORT ${PORT}`));
+const server = app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
 
 const io = require('socket.io')(server);
 
